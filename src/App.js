feca14eebd435ebf8e2962ebea4f8e04bf3e84d5
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-09-21 21:35:15 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
 
-  const testData = [
+  const data = [
     "I change my locks every 16 days.",
     "Creativity is for people with glasses who like to lie.",
     "Cultivating a manly musk puts opponent on notice.",
   ];
 
-  const [data, setData] = useState(testData);
-
   const listArray = data.map(item => {
     return (<li>{item}</li>);
   });
 
-  
+  const clear = function () {
+    data.length = 0;
+  };
+
   return (
     <div className="App">
       <h3>Quote Generator - React</h3>
 
       <button type="button">Add Item</button>
       <span>&nbsp;</span>
-      <button id="clear" type="button">Clear List</button>
+      <button id="clear" type="button" onClick={clear}>Clear List</button>
 
       <ul>
         {listArray}
