3901336b1a98dfb15da074738f83e6931c331fab
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-09-23 14:00:37 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  console.log("Rendering Component", new Date().getTime());
+
   return (
     <div className="App">
       <h3>Quote Generator - React</h3>
-      
+
     </div>
   );
 }
