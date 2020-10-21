dadee03e71e1550927ca08a745cfaf982e51fb87
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2020-10-20 19:15:38 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
+const data = [
+  "I change my locks every 16 days.",
+  "Creativity is for people with glasses who like to lie.",
+  "Cultivating a manly musk puts opponents on notice.",
+];
+
 function App() {
   console.log("Rendering App Component", new Date().getTime());
-
-  const data = [
-    "I change my locks every 16 days.",
-    "Creativity is for people with glasses who like to lie.",
-    "Cultivating a manly musk puts opponents on notice.",
-  ];
 
   const listArray = [];
   for (const item of data) {
